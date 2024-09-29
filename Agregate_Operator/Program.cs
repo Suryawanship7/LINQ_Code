4cb288baff90d8e2ab16e8fddcdb7f1b6b6b117b
Suryawanship7 <parth7112000@gmail.com>
2024-09-29 10:09:08 +0530

    adding new project

---
+++ Agregate_Operator/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+List<int>Number= new List<int>() { 1,2,3,4,5,6,7,8};
+//Agregaet fun max
+int num = Number.Max();
+Console.WriteLine($"max no in list:{num}");
+//Min func
+num= Number.Min();
+Console.WriteLine($"Min No in list :{num}");
+//average func
+var avg=Number.Average();
+Console.WriteLine($"Average of list is:{avg}");
+//Count
+int count= Number.Count();
+Console.WriteLine($"count of list{count}");
+//longCount
+long Count= Number.LongCount();
+Console.WriteLine($"Long count:{count}");
+//sum or summation
+num= Number.Sum();
+Console.WriteLine($"Sum of ele of list is:{num}");
+//aggregate
+var Num = Number.Aggregate((i,j)=>i*j);
+Console.WriteLine($"Aggregate of fun:{Num}");
+Console.ReadLine();
---
+++ Quantifiers/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+List<int> Numbers = new List<int>() { 1,2,3,4,5,6,7,8,9 };
+//any it return in treu or false it is used to find check casses
+bool b = Numbers.Any(x => x >= 5 && x <= 7);
+Console.WriteLine($"bool b = Numbers.Any(x => x >= 5 && x <= 7):{b}");
+//all condn it is used in such cases all element are eligble for given condn are true then itb will give true or else false
+b = Numbers.All(x => x % 2 == 0);
+Console.WriteLine($"b = Numbers.All(x => x % 2 == 0):{b}");
+//contains it will check condn is satisfing or not
+b = Numbers.Contains(3);
+Console.WriteLine($"Numbers.Contains(3):{b}");
+
+
+
+
+
+
+
+
+
+Console.ReadLine();
